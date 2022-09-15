1305138758787215800ba33c40cc6931d5ac20f8
richelbilderbeek <richel@richelbilderbeek.nl>
2022-09-15 13:55:18 +0200

    Describe variables

--- create_test_data.R
+++ create_test_data.R
@@ -19,6 +19,7 @@ for (i in seq_len(n_characters)) {
   t$p_clock_street <- runif(n = nrow(t), min = 0.5, max = 0.7)
   t$p_key_street <- runif(n = nrow(t), min = 0.5, max = 0.6)
   t$p_market_street <- runif(n = nrow(t), min = 0.4, max = 0.7)
+  t$p_correct_final_choice <- 1.0
   t$p_wrong_final_choice_1 <- runif(n = nrow(t), min = exp(-t$t), max = 0.1)
   t$p_wrong_final_choice_2 <- runif(n = nrow(t), min = exp(-t$t), max = 0.1)
   
